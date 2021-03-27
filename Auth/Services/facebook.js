e391ea2e7ece6249c574dc6a16de51b19f1b4d31
JustBeingAbdi <lg.tv.her.er.jeg@yandex.com>
2021-03-27 07:58:22 +0000

    + Facebook ouath

--- Auth/Services/facebook.js
+++ Auth/Services/facebook.js
@@ -2,15 +2,6 @@ let queryString = require("query-string");
 let config = require("../../config.json");
 
 module.exports.GenerateFUrl = async(state) => {
-const stringifiedParams = queryString.stringify({
-  client_id: config.facebook_clientID,
-  redirect_uri: 'https://ouath.openauth.cf/facebook/callback',
-  scope: ['email'].join(','), // comma seperated string
-  response_type: 'code',
-  auth_type: 'rerequest',
-  display: 'popup',
-  state: state
-  
-});
-return `https://www.facebook.com/v4.0/dialog/oauth?${stringifiedParams}`
+
+return `https://www.facebook.com/v10.0/dialog/oauth?client_id=${config.facebook_clientID}&redirect_uri=https://ouath.openauth.cf/facebook/callback&state=${state}&response_type=code`
 }
